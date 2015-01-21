31ba85619cb4d5e16443bec80753064cc5ba1d74
thehappycactus <thehappycactus@gmail.com>
2015-01-20 20:22:43 -0500

    Twitter Updates

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,10 +1,23 @@
 twttr.events.bind(
   'tweet',
-  function (event) {
-    // Do something there
-    console.log('tweet occurred!');
-    console.log('cookies: ' + document.cookie);
-  }
+	function (event) {
+	    console.log('cookies: ' + document.cookie);
+	    App.tweetSuccess = true;
+
+	    var pageData = {
+	    	specialTitle: App.special.fDeal,
+			image: App.constants.IMG_LOC + App.special.image,
+			startTime: moment.utc(App.special.fStartTimeUTC).local(),
+			endTime: moment.utc(App.special.fStopTimeUTC).local(),
+			oauthSuccess: App.oauthSuccess,
+			tweetSuccess: App.tweetSuccess
+	    }
+
+	    var midHtml = midTemplate(pageData);
+	    $('.main').html(midHtml);
+
+	    Twitter.confirmTweet();
+	}
 );
 
 App = {
@@ -14,19 +27,22 @@ App = {
 	topTemplate: null,
 	midTemplate: null,
 	oauthSuccess: false,
+	tweetSuccess: false,
 	oauth_verifier: null,
 	oauth_token: null,
-	specialCallback: function(merchantID) {
+	getMerchant: function(merchantID) {
 		var self = this;
 		Services.getMerchant(merchantID).then(
 			function(data) {
 				if (data) {
 					App.merchant = data;
-					pageData = {
-						specialTitle: App.special.fDeal,
-						image: self.constants.IMG_LOC + App.special.image,
-						startTime: self.special.fStartTimeUTC,
-						endTime: self.special.fStopTimeUTC,
+					var pageData = {
+						specialTitle: self.special.fDeal,
+						image: self.constants.IMG_LOC + self.special.image,
+						startTime: moment.utc(self.special.fStartTimeUTC).local(),
+						endTime: moment.utc(self.special.fStopTimeUTC).local(),
+						oauthSuccess: self.oauthSuccess,
+						tweetSuccess: self.tweetSuccess
 					}
 					var topHtml = topTemplate({name: data.name});
 					var midHtml = midTemplate(pageData);
@@ -55,17 +71,47 @@ $(document).ready(function() {
 	topTemplate = Handlebars.compile(topSource);
 	midTemplate = Handlebars.compile(midSource);
 
+	// If the user has successfully authorized our app...
 	if (code.indexOf('success') !== -1) {
-		this.oauthSuccess = true;
-		oauth_token = vars[2].substring(0, 32);
-		App.oauth_verifier = vars[3];
-		Twitter.oAuthAccess();
-	} else {
+		// 1. Get the oauth token & token secret saved into cookies, use to call getUserInfo
+		/*App.oauth_token = vars[2].substring(0, 32);*/
+		document.cookie = "verifier=" + vars[3];
+		//App.oauth_verifier = vars[3];
+		App.oauthSuccess = true;
+		Twitter.getUserInfo();
+
+		// 2. Get the special code saved into cookies to pull down the Special
+		var cookies = document.cookie.split(';');
+		for (var i = 0; i < cookies.length; i ++) {
+			if (cookies[i].indexOf('specialCode') !== -1) {
+				code = cookies[i].substring(12);
+				break;
+			}
+		}
+
+		// 3. Get the Special & Merchant objects, & get Twitter User's information
+		Services.getSpecial(code).then(
+			function(data) {
+				if (data) {
+					App.special = data;
+					App.getMerchant(data.placeID);
+				}
+			},
+			function(error) {
+
+			});
+
+
+	} else {	// Else...
+		// 1. Save the special code into a cookie for page reload
+		document.cookie = "specialCode=" + code;
+
+		// 2. Get Special & Merchant objects for display
 		Services.getSpecial(code).then(
 		function(data) {
 			if (data) {
 				App.special = data;
-				App.specialCallback(data.placeID)
+				App.getMerchant(data.placeID)
 			}
 		},
 		function(error) {
--- WebContent/js/twitter.js
+++ WebContent/js/twitter.js
@@ -1,24 +1,4 @@
 Twitter = {
-	TWITTER_TEST_CONSUMER_KEY: "zyeWSvwTzh7byVy9cAIfQsxql",
-	TWITTER_TEST_CONSUMER_SECRET: "BSb5yVPqNsAi7CtOY9AzcVNwf9hjIzS3TPDe4Ep0RB9PvKTIKn",
-	TWITTER_TEST_ACCESS_TOKEN: "2870148905-33AsepCQFe9t7wJoLDY6lMEipyIm7u1sINmPMQZ",
-	TWITTER_TEST_ACCESS_SECRET: "CWvfcmf4NY1VaLohiQztUuJIRlRMCEJSJ1fN33k8aVJSX",
-	TWITTER_TEST_ID: '2870148905l',
-
-	// IRL Data
-	/*TWITTER_TEST_CONSUMER_KEY: "G8A0SlEYiwKD4ydEQeYIe3zZE",
-	TWITTER_TEST_CONSUMER_SECRET: "gjWb2qxPOlzEyKUodNlkrDO5ZZbTwfpcDuY7umRCH9UsH3lfVT",
-	TWITTER_TEST_ACCESS_TOKEN: "1649497638-1BjjkkhqqfWPsEvseUAJHAiVzoCDJSAlYTcvktN",
-	TWITTER_TEST_ACCESS_SECRET: "gn92qQ53n0s6k07HT0Rb7QYHjLGGFYU9Q5AQyLVMHWMYT",
-	TWITTER_TEST_ID: '1649497638',*/
-
-	/*genNonce: function() {
-		var chars = '01234567890abcdefghijklmnopqrstuvwxyzABCDEFTHIJKLMNOPQRSTUVWXYZ';
-	    var result = '';
-	    for (var i = 32; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-	    return result;
-	},*/
-
 	oAuthRequest: function() {
 
 		$.ajax({
@@ -35,19 +15,21 @@ Twitter = {
 			}
 		});
 	},
-	oAuthAccess: function() {
+	getUserInfo: function() {
 		var cookies = document.cookie.split(';');
-		var oauthToken, oauthSecret;
-		var yesterday = moment().subtract(1, 'days').utc();
+		var oauthToken, oauthSecret, verifier;
+		//var yesterday = moment().subtract(1, 'days').utc();
 
 		// Get the token & secret, the remove both cookies
 		for (var i = 0; i < cookies.length; i ++) {
 			if (cookies[i].indexOf('tokenSecret') !== -1) {
 				oauthSecret = cookies[i].substring(13);
-				document.cookie = "tokenSecret=; expires" + yesterday;
+				//document.cookie = "tokenSecret=; expires" + yesterday;
 			} else if (cookies[i].indexOf('token') !== -1) {
-				oauthToken = cookies[i].substring(6);
-				document.cookie = "token=; expires=" + yesterday;
+				oauthToken = cookies[i].substring(7);
+				//document.cookie = "token=; expires=" + yesterday;
+			} else if (cookies[i].indexOf('verifier') !== -1) {
+				verifier = cookies[i].substring(10);
 			}
 		}
 
@@ -55,73 +37,46 @@ Twitter = {
 			url: 'http://localhost:8080/JoinMeAt_v2/rs/Twitter/user',
 			type: 'POST',
 			dataType: 'json',
-			data: { verifier: App.oauth_verifier, token: oauthToken, tokenSecret: oauthSecret },
+			data: { verifier: verifier, token: oauthToken, tokenSecret: oauthSecret },
 			success: function(data) {
 				var User = data;
 				App.User = data;
-				// from here we need to send the tweet
-				// we *could* insert the tweet intent here, but then it won't capture the tweet event
 			} ,
 			error: function(error) {
 				console.log('total failure, noob');
 			}
 		});
-	}
+	},
+	confirmTweet: function() {
+		var cookies = document.cookie.split(';');
+		var oauthToken, oauthSecret;
 
+		// Get the token & secret, the remove both cookies
+		for (var i = 0; i < cookies.length; i ++) {
+			if (cookies[i].indexOf('tokenSecret') !== -1) 
+				oauthSecret = cookies[i].substring(13);
+			else if (cookies[i].indexOf('token') !== -1) 
+				oauthToken = cookies[i].substring(7);
+			else if (cookies[i].indexOf('verifier') !== -1) 
+				verifier = cookies[i].substring(10);
+		}
 
-		/*var oauth = OAuth({
-		    consumer: {
-		        public: this.TWITTER_TEST_CONSUMER_KEY,
-		        secret: this.TWITTER_TEST_CONSUMER_SECRET
-		    },
-		    signature_method: 'HMAC-SHA1'
-		});
-
-		var token = {
-		    public: this.TWITTER_TEST_ACCESS_TOKEN,
-		    secret: this.TWITTER_TEST_ACCESS_SECRET
-		};
-
-		var request_data = {
-		    url: 'https://api.twitter.com/oauth/request_token',
-		    method: 'POST'
-		};
-
-		var auth = oauth.authorize(request_data, token);
-		var authHeader = oauth.toHeader(auth);
-
-		$.ajax({
-		    url: request_data.url,
-		    type: request_data.method,
-		    data: request_data.data,
-		    dataType: 'json',
-		    headers: authHeader
-		}).done(function(data) {
-			console.log('done data: ' + data);
-		    //process your data here
-		});*/
-
-		/*var auth = oauth.authorize(requestData, token);
-		var testHeader = oauth.toHeader(auth);
-
-		$.ajax({
-			url: requestData.url,
-			type: requestData.method,
+		return $.ajax({
+			url: 'http://localhost:8080/JoinMeAt_v2/rs/Twitter/confirm',
+			type: 'POST',
 			dataType: 'json',
-			//headers: oauth.toHeader(oauth.authorize(requestData, token)),
-			beforeSend: function(xhr) {
-				xhr.setRequestHeader('Authorization', testHeader.Authorization);
-				console.log('Authorization: ' + testHeader.Authorization);
+			data: { 
+				verifier: verifier, 
+				token: oauthToken, 
+				tokenSecret: oauthSecret,
+				username: App.User.screenName },
+			success: function(data) {
+				console.log('twitter confirm successfully returned');
 			},
-			success: function(data, status, xhr) {
-				console.log('success!');
-			},
-			error: function(xhr, status, errorThrown) {
-				console.log('error: ' + errorThrown);
+			error: function(error) {
+				console.log('twitter confirm error');
 			}
-		}).done(function(data) {
-			console.log("all!");
-		});
 
-	}*/
+		})
+	}
 }
