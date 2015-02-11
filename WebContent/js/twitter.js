1ddedf011bb47588286c2d544b4de80653512b01
thehappycactus <thehappycactus@gmail.com>
2015-02-11 10:36:42 -0500

    Abstracted out getCookies
    Abstracted out the code to obtain cookies to Util.getCookies

--- WebContent/js/twitter.js
+++ WebContent/js/twitter.js
@@ -19,34 +19,22 @@ Twitter = {
 		});
 	},
 	getUserInfo: function() {
-		var cookies = document.cookie.split(';');
-		var oauthToken, oauthSecret, verifier;
-
-		// Get the token & secret, the remove both cookies
-		for (var i = 0; i < cookies.length; i ++) {
-			if (cookies[i].indexOf('tokenSecret') !== -1) {
-				oauthSecret = cookies[i].substring(13);
-			} else if (cookies[i].indexOf('token') !== -1) {
-				oauthToken = cookies[i].substring(7);
-			} else if (cookies[i].indexOf('verifier') !== -1) {
-				verifier = cookies[i].substring(10);
-
-			}
-		}
-
-		$.ajax({
-			url: App.constants.URL_GSON + 'Twitter/user',
-			//url: 'http://localhost:8080/JoinMeAt_v2/rs/Twitter/user',
-			type: 'POST',
-			dataType: 'json',
-			data: { verifier: verifier, token: oauthToken, tokenSecret: oauthSecret },
-			success: function(data) {
-				App.User = data;
-			},
-			error: function(error) {
-				console.log('total failure, noob');
-			}
-		});
+		Util.getCookies().then( function(cookies) {
+			$.ajax({
+				//url: App.constants.URL_GSON + 'Twitter/user',
+				url: 'http://localhost:8080/JoinMeAt_v2/rs/Twitter/user',
+				type: 'POST',
+				dataType: 'json',
+				data: { verifier: cookies.verifier, token: cookies.oauthToken, tokenSecret: cookies.oauthSecret },
+				success: function(data) {
+					App.User = data;
+				},
+				error: function(error) {
+					console.log('total failure, noob');
+				}
+			});
+		},
+	function() { /* do nothing */ });
 	},
 	statusUpdate: function(_status) {
 		var cookies = document.cookie.split(';');
@@ -83,7 +71,39 @@ Twitter = {
 			}
 		});
 	},
-	confirmTweet: function() {
+	getFriends: function(_query) {
+
+		Util.getCookies().then(
+			function(cookies) {
+				//_query = '	tuft';
+				_query = '';
+
+				return $.ajax({
+					//url: App.constants.URL_GSON + 'Twitter/confirm',
+					url: 'http://localhost:8080/JoinMeAt_v2/rs/Twitter/users/search',
+					type: 'POST',
+					dataType: 'json',
+					data: { 
+						token: cookies.oauthToken, 
+						tokenSecret: cookies.oauthSecret, 
+						verifier: cookies.verifier,
+						query: _query,
+					},
+					success: function(data) {
+						console.log('confirmTweet: ' + data);
+						if (data > 0)
+							return true
+						else 
+							return false;
+					},
+					error: function(error) {
+						return false;
+					}
+				});
+			},
+			function(error) { /* do nothing */ });
+	}
+	/*confirmTweet: function() {
 
 		return $.ajax({
 			url: App.constants.URL_GSON + 'Twitter/confirm',
@@ -102,5 +122,5 @@ Twitter = {
 				return false;
 			}
 		});
-	}
+	}*/
 }
--- WebContent/js/util.js
+++ WebContent/js/util.js
@@ -11,6 +11,32 @@ Util = {
 
 		Services.logWebAction(code, App.special.placeID, 1);
 	},
+	getCookies: function() {
+		var deferred = $.Deferred();
+
+		var cookies = document.cookie.split(';');
+		var oauthToken, oauthSecret, verifier;
+
+		// Get the token & secret, the remove both cookies
+		for (var i = 0; i < cookies.length; i ++) {
+			if (cookies[i].indexOf('tokenSecret') !== -1) {
+				oauthSecret = cookies[i].substring(13);
+			} else if (cookies[i].indexOf('token') !== -1) {
+				oauthToken = cookies[i].substring(7);
+			} else if (cookies[i].indexOf('verifier') !== -1) {
+				verifier = cookies[i].substring(10);
+
+			}
+		}
+
+		deferred.resolve({ 
+			oauthToken: oauthToken, 
+			oauthSecret: oauthSecret,
+			verifier: verifier
+		});
+
+		return deferred.promise();
+	},
 	validateTweet: function(tweet) {
 		var deferred = $.Deferred();
 
